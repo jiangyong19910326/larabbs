16ae71b22a9bff0e47275407a0ccdbd8b64ab915
vagrant <vagrant@homestead>
2018-07-15 04:02:15 +0000

    编辑个人资料功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use App\Http\Requests\UserRequest;
 
 /**
  * Class UsersController
@@ -20,5 +20,21 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    /**
+     * @用户修改页面
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 }
