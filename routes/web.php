25e261e0a9c9544560de90765b43ad42813bec67
vagrant <vagrant@homestead>
2018-08-14 07:51:43 +0000

    站点首页

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //Auth::routes(); // 这是用户认证的laravel路由，等同于下面
 // Authentication Routes...
